344bd2a29f2d6876bda08b48688af2187e1b1843
Driss Amri <driss.amri@gmail.com>
2020-04-03 14:25:06 +0200

    Java 8 & Java 11

---
+++ java11/src/main/java/be/drissamri/performance/services/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package be.drissamri.performance.services;
+
+
+import be.drissamri.performance.model.Product;
+import be.drissamri.performance.model.ProductRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ProductServiceImpl implements ProductService {
+    private final DynamoDbClient dynamoDbClient;
+
+    @Inject
+    public ProductServiceImpl(DynamoDbClient dynamoDbClient) {
+        this.dynamoDbClient = dynamoDbClient;
+    }
+
+    @Override
+    public Product addProduct(final ProductRequest productRequest) {
+        final String productId = UUID.randomUUID().toString();
+
+        final Map<String, AttributeValue> itemValues = new HashMap<>();
+        itemValues.put("id", AttributeValue.builder().s(productId).build());
+        itemValues.put("name", AttributeValue.builder().s(productRequest.getName()).build());
+
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName("productsv11")
+                .item(itemValues)
+                .build();
+
+        try {
+            this.dynamoDbClient.putItem(request);
+
+            return new Product(productId, productRequest.getName());
+        } catch (Exception e) {
+            throw new RuntimeException("Database update failed!");
+        }
+    }
+}
